7c009b8236cac3763b52725ca4804dffbae91e53
NkawaK <kentawest83@gmail.com>
2018-11-12 22:44:45 +0900

    add signup user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = 'ユーザーを登録しました！'
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get 'signup_path', to:'users#new'
+  post '/signup_path', to: 'users#create'
   resources :users
 end
