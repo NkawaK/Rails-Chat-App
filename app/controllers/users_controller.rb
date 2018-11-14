62f8e95717ebc753bff2363aa95f3eaa3c4d12c5
NkawaK <kentawest83@gmail.com>
2018-11-14 23:41:12 +0900

    add login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'ユーザーを登録しました！'
+      flash.now[:success] = 'ユーザーを登録しました！'
       redirect_to @user
     else
       render 'new'
