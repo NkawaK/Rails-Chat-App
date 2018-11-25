bb719ae67275d38783b09da9ccab17307673e7ad
NkawaK <kentawest83@gmail.com>
2018-11-25 23:27:42 +0900

    add message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
+  has_many :messages, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_122820) do
+ActiveRecord::Schema.define(version: 2018_11_25_142447) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "room_users", force: :cascade do |t|
     t.integer "user_id"
